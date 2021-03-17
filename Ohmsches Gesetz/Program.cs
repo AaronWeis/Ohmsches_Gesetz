ce2b68ef844c6cf9f33450b2fb99ea9e0627adc0
Aaron Weis <tuba.aaron@gmail.com>
2021-03-17 11:08:56 +0100

    Initial commit

---
+++ Ohmsches Gesetz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ohmsches_Gesetz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("R1 (in Ohm): ");
+            string userInput = Console.ReadLine();
+            int r1 = Convert.ToInt32(userInput);
+            
+            Console.Write("R2 (in Ohm): "); 
+            userInput = Console.ReadLine();
+            int r2 = Convert.ToInt32(userInput);
+            
+            Console.WriteLine("Reihenschaltung: " + (r1 + r2) + " Ohm");
+            Console.WriteLine("Parallelschaltung: " + ((r1 * r2) / (r1 + r2)) + " Ohm");
+        }
+    }
+}
